65ad4d50b9d60972a14698c5bb920e4a7451b54f
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2024-12-23 23:30:23 +0530

    Update : Integrate Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from "./prisma.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(5000);
 }
-bootstrap();
+bootstrap().then(() => {
+  // Add your code here if needed
+});
