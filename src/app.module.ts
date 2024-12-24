58d03c2d24eea564a6290910201c0034d459efae
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2024-12-24 13:17:44 +0530

    add : user-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from "./prisma.service";
 import { ConfigModule } from "@nestjs/config";
+import { UserController } from './user/user.controller';
+import { UserModule } from "./user/user.module";
+import { UserService } from "./user/user.service";
 
 @Module({
-  imports: [ConfigModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ConfigModule, UserModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   await app.listen(5000);
