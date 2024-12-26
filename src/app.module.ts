fe321edb5c2637ab40c5c5c4ff2bbd327d19e829
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2024-12-26 14:10:51 +0530

    add:) application-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from "./prisma.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { UserController } from './user/user.controller';
-import { UserModule } from "./user/user.module";
-import { UserService } from "./user/user.service";
+import { PrismaService } from './prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 import { CompanyModule } from './company/company.module';
-import { PassportModule } from "@nestjs/passport";
-import { JwtModule } from "@nestjs/jwt";
-import { ApplicationModule } from './application/application.module';
 import { JobModule } from './job/job.module';
-import { ApplicationService } from "./application/application.service";
+import { ApplicationModule } from './application/application.module';
+import { ApplicationService } from './application/application.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [  PassportModule.register({ defaultStrategy: 'jwt' }),
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -22,8 +23,20 @@ import { ApplicationService } from "./application/application.service";
         signOptions: { expiresIn: '1d' },
       }),
       inject: [ConfigService],
-    }),ConfigModule, UserModule, CompanyModule, ApplicationModule, JobModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, PrismaService, ApplicationService],
+    }),
+    ConfigModule,
+    UserModule,
+    CompanyModule,
+    JobModule,
+    ApplicationModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    UserService,
+    JwtStrategy,
+    PrismaService,
+    ApplicationService,
+  ],
 })
 export class AppModule {}
